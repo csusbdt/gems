3e8a78727ba20f5b3946e72567c71d5f1d77cdee
David Turner <csusbdt@gmail.com>
2013-11-12 00:15:55 -0800

    X

--- scripts/createdb.sh
+++ scripts/createdb.sh
@@ -1,17 +1,16 @@
-export DB=http://admin:1234@localhost:5984/gems
-curl -X DELETE ${DB}
-curl -X PUT    ${DB}
-curl -X PUT    ${DB}/_security -H "Content-type: application/json" -d @security.json
+# S = server url
+export S=http://admin:1234@localhost:5984
 
-curl -X POST   ${DB} -H "Content-type: application/json" -d '{"_id": "a", "pw": "a", "balance": 10, "gems": 0 }'
-curl -X POST   ${DB} -H "Content-type: application/json" -d '{"_id": "b", "pw": "b", "balance": 0, "gems": 0 }'
+# delete existing databases if any
+curl -X DELETE ${S}/users
 
-curl -X GET    ${DB}/a
+# create database "users"
+curl -X PUT    ${S}/users
 
-curl -X PUT    ${DB}/a -H "Content-type: application/json" -d '{"_id": "a", "_rev": "1-19bb9ed0bf4f53418d8ec26b19c0df37", "pw": "a", "balance": 5, "gems": 0 }'
+# set admin as sole authorized user
+curl -X PUT    ${S}/users/_security -H "Content-type: application/json" -d @security.json
 
-curl -X GET    ${DB}/a
+# create 2 users
+curl -X POST   ${S}/users -H "Content-type: application/json" -d '{"_id": "a", "pw": "a", "balance": 5, "gems": 0 }'
+curl -X POST   ${S}/users -H "Content-type: application/json" -d '{"_id": "b", "pw": "b", "balance": 0, "gems": 0 }'
 
-curl -X PUT    ${DB}/a -H "Content-type: application/json" -d '{"_id": "a", "_rev": "1-19bb9ed0bf4f53418d8ec26b19c0df37", "pw": "a", "balance": 100, "gems": 0 }'
-
-curl -X GET    ${DB}/a
