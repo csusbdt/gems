dbc76c56f318d53b22a093248161130e47b3739d
David Turner <csusbdt@gmail.com>
2013-11-12 20:40:03 -0800

    refactoring

--- scripts/createdb.sh
+++ scripts/createdb.sh
@@ -8,9 +8,13 @@ curl -X DELETE ${S}/users
 curl -X PUT    ${S}/users
 
 # set admin as sole authorized user
-curl -X PUT    ${S}/users/_security -H "Content-type: application/json" -d @security.json
+curl -X PUT    ${S}/users/_security -H "Content-type: application/json" \
+               -d @security.json
 
 # create 2 users
-curl -X POST   ${S}/users -H "Content-type: application/json" -d '{"_id": "a", "pw": "a", "balance": 5, "gems": 0 }'
-curl -X POST   ${S}/users -H "Content-type: application/json" -d '{"_id": "b", "pw": "b", "balance": 0, "gems": 0 }'
+curl -X POST   ${S}/users -H "Content-type: application/json"           \
+               -d '{"_id": "a", "pw": "a", "balance": 5, "gems": 0 }'
+
+curl -X POST   ${S}/users -H "Content-type: application/json"           \
+               -d '{"_id": "b", "pw": "b", "balance": 0, "gems": 0 }'
 
